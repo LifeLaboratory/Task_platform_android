c35b45dbeb598ff70c5c6e442a0caf417588610a
alexandravg <a.efimova09@gmail.com>
2019-01-19 22:50:45 +0700

    добавлено повторение пароля

--- app/src/main/java/ru/lifelaboratory/task_platform/RegisterActivity.kt
+++ app/src/main/java/ru/lifelaboratory/task_platform/RegisterActivity.kt
@@ -5,6 +5,7 @@ import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 
 import kotlinx.android.synthetic.main.activity_register.*
 
@@ -19,6 +20,12 @@ class RegisterActivity : AppCompatActivity() {
             val login = findViewById<TextView>(R.id.login).text.toString()
             val email = findViewById<TextView>(R.id.email).text.toString()
             val pass = findViewById<TextView>(R.id.password).text.toString()
+            val repeatPass = findViewById<TextView>(R.id.reppass).text.toString()
+            if(pass.equals(repeatPass) == false){
+                Toast.makeText(this, "Пароли не совпадают", Toast.LENGTH_SHORT).show()
+                findViewById<TextView>(R.id.password).text = ""
+                findViewById<TextView>(R.id.reppass).text = ""
+            }
             //отправить запрос с этими всеми данными на сервер
         }
     }
